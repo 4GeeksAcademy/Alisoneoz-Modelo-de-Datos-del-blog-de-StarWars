1010be8defc6c5538fb3e5b76a0f268eaa6f45a0
alisoneoz <alisonestephanyoz@gmail.com>
2025-04-30 18:36:37 -0400

    Add tablas intermedias

--- src/models.py
+++ src/models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
+from sqlalchemy import String, 
 from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
@@ -47,4 +47,28 @@ class Characters(db.Model):
             "id": self.id,
             "name":self.name,
             "color_de_ojos": self.color_de_ojos
+        }
+
+class TablaIntermediaDePersonajesFavoritos(db.Model):
+    __tablename__="tabla_intermedia_de_personajes_favoritos"
+
+    user_id: Mapped[int] = mapped_column('userID', db.ForeignKey('user.userID'), primary_key=True)
+    character_id: Mapped[int] = mapped_column('characterID', db.ForeignKey('characters.characterID', primary_key=True))
+
+    def serialize(self):
+        return{
+            "user_id":self.user_id,
+            "character_id":self.character_id,
+        }
+
+class TablaIntermediaDePlanetasFavoritos(db.Model):
+    __tablename__="tabla_intermedia_de_planetas_favoritos"
+
+    user_id: Mapped[int] = mapped_column('userID', db.ForeignKey('user.userID'), primary_key=True)
+    planet_id: Mapped[int] = mapped_column('planetID', db.ForeignKey('planets.planetID', primary_key=True))
+
+    def serialize(self):
+        return{
+            "user_id": self.user_id,
+            "planet_id": self.planet_id,
         }
