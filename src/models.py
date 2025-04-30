e135271d84f548b5dfd6990cd870d7102db03e98
alisoneoz <alisonestephanyoz@gmail.com>
2025-04-30 18:21:29 -0400

    initial commit

--- src/models.py
+++ src/models.py
@@ -5,15 +5,46 @@ from sqlalchemy.orm import Mapped, mapped_column
 db = SQLAlchemy()
 
 class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
+    __tablename__ = "user"
+
+    id: Mapped[int] = mapped_column('userID',primary_key=True, autoincrement=True)
+    username: Mapped[str] = mapped_column(String(80), unique=True, nullable=False)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
-
+    password: Mapped[str] = mapped_column(String(120),nullable=False)
+    
     def serialize(self):
         return {
             "id": self.id,
+            "username":self.username,
             "email": self.email,
             # do not serialize the password, its a security breach
+
+        }
+
+class Planets(db.Model):
+    __tablename__="planets"
+
+    id: Mapped[int] = mapped_column ('planetID', primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(80), unique=True, nullable=False)
+    clima: Mapped[str] = mapped_column(String(80), nullable=True)
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "name":self.name,
+            "clima": self.clima
+        }
+
+class Characters(db.Model):
+    __tablename__="characters"
+
+    id: Mapped[int] = mapped_column ('characterID', primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(80), unique=True, nullable=False)
+    color_de_ojos: Mapped[str] = mapped_column(String(80), nullable=True)
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "name":self.name,
+            "color_de_ojos": self.color_de_ojos
         }
